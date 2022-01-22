123fd97853c9e9cba88d1465fa96b2cb7b0b24dd
Valentin181988 <85246610+Valentin181988@users.noreply.github.com>
2022-01-22 17:36:19 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,89 @@
-export const App = () => {
-  return <>React homework template</>;
-};
+import React, { Component } from "react";
+import { nanoid } from 'nanoid';
+import { PhoneBookTitle } from "./PhoneBookTitle/PhoneBookTitle";
+import { PhoneBookForm } from './PhoneBookForm/PhoneBookForm';
+import { ContactsList } from './ContactsList/ContactsList';
+import { ContactsTitle } from "./ContactsTitle/ContactsTitle";
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: ''
+  };
+
+  formSubmitHandler = ({name, number}) => {
+
+    const alreadyInList = this.state.contacts.find(contact => contact.name === name);
+
+    if (alreadyInList !== undefined) {
+        alert(`${name} is already in contacts`);
+
+        return;
+    };
+
+    const contact = {
+      id: nanoid(),
+      name,
+      number
+    };
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts]
+    }))
+  };
+
+  changeSearchFilter = event => {
+    this.setState({filter: event.currentTarget.value})
+  };
+
+  getVisibleContacts = () => {
+    const {contacts, filter} = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+      };
+    });
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const contactsParsed = JSON.parse(contacts);
+
+    if (contactsParsed) {
+      this.setState({contacts: contactsParsed});
+    };
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+     if (this.state.contacts !== prevState.contacts) {
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+     };
+  };
+
+  render() {
+
+     const { filter } = this.state;
+
+      const searchContact = this.getVisibleContacts();
+
+      return (
+        <div>
+          <PhoneBookTitle title="Phone book"/>
+          <PhoneBookForm /* onSubmit={this.formSubmitHandler} *//>
+          <ContactsTitle title="Contacts"/>
+          <Filter value={filter} onChange={this.changeSearchFilter}/>
+          <ContactsList contacts={searchContact} onDeleteContact={this.deleteContact}/>
+        </div>
+    );
+  };
+
+}
---
+++ src/components/PhoneBookForm/PhoneBookForm.jsx
@@ -0,0 +1,132 @@
+import { useState } from "react";
+/* import PropTypes from 'prop-types'; */
+import { nanoid } from 'nanoid';
+
+import { Form, Label, Input, SubmitButton } from "./PhoneBookForm.styled";
+
+export function PhoneBookForm() {
+
+  const nameInputId = nanoid();
+  const numberInputId = nanoid();
+
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = event => {
+
+    const {name, value} = event.target
+    
+    switch(name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default: return;
+    };
+  };
+
+
+  return (
+    <Form /* onSubmit={this.handleSubmit} */>
+        <Label htmlFor={nameInputId}>Name</Label>
+        <Input
+            type="text"
+            name="name"
+            id={nameInputId}
+            value={name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={handleChange}/>
+
+        <Label htmlFor={numberInputId}>Number</Label>
+        <Input
+            type="number"
+            name="number"
+            id={numberInputId}
+            value={number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={handleChange}/>
+        <SubmitButton
+            type="submit"
+            /* disabled={!this.state.number || !this.state.name} */>Add contact
+        </SubmitButton>
+  </Form>
+);
+};
+
+/* export class PhoneBookForm extends Component {
+
+    state = {
+        name: '',
+        number: ''
+      };
+
+    nameInputId = nanoid();
+    numberInputId = nanoid();
+
+    handleInputChange = event => {
+        const {name, value} = event.currentTarget;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state);
+        this.resetForm();
+    };
+
+    resetForm = () => {
+        this.setState({name: '', number: ''})
+    };
+
+    render() {
+        const {name, number} = this.state;
+
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Label htmlFor={this.nameInputId}>Name</Label>
+                <Input
+                    type="text"
+                    name="name"
+                    id={this.nameInputId}
+                    value={name}
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                    onChange={this.handleInputChange}/>
+
+                <Label htmlFor={this.numberInputId}>Number</Label>
+                <Input
+                    type="number"
+                    name="number"
+                    id={this.numberInputId}
+                    value={number}
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                    onChange={this.handleInputChange}/>
+                <SubmitButton
+                    type="submit"
+                    disabled={!this.state.number || !this.state.name}>Add contact
+                </SubmitButton>
+          </Form>
+        );
+    };
+
+};
+
+PhoneBookForm.propTypes = {
+    onSubmit: PropTypes.func
+ }; */
+
+
